62835031909796a014095eec93d73d1318dd1c19
Twoot <inenquser@gmail.com>
2026-08-26 15:48:17 +0300

    Create lru cache decorator

---
+++ src/cache_decorator.py
@@ -0,0 +1,68 @@
+import unittest.mock
+from functools import wraps
+from collections import OrderedDict
+
+
+def lru_cache(*args, **kwargs):
+    maxsize = kwargs.get("maxsize", 128)
+
+    def decorator(func):
+        cache = OrderedDict()
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            key = (args, tuple(sorted(kwargs.items())))
+            if key in cache:
+                cache.move_to_end(key)
+                return cache[key]
+            result = func(*args, **kwargs)
+
+            if len(cache) >= maxsize:
+                cache.popitem(last=False)
+            cache[key] = result
+            return result
+
+        return wrapper
+
+    if len(args) == 1 and callable(args[0]) and not kwargs:
+        return decorator(args[0])
+    return decorator
+
+
+
+@lru_cache
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+@lru_cache
+def sum_many(a: int, b: int, *, c: int, d: int) -> int:
+    return a + b + c + d
+
+
+@lru_cache(maxsize=3)
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+if __name__ == '__main__':
+    assert sum(1, 2) == 3
+    assert sum(3, 4) == 7
+
+    assert multiply(1, 2) == 2
+    assert multiply(3, 4) == 12
+
+    assert sum_many(1, 2, c=3, d=4) == 10
+
+    mocked_func = unittest.mock.Mock()
+    mocked_func.side_effect = [1, 2, 3, 4]
+
+    decorated = lru_cache(maxsize=2)(mocked_func)
+    assert decorated(1, 2) == 1
+    assert decorated(1, 2) == 1
+    assert decorated(3, 4) == 2
+    assert decorated(3, 4) == 2
+    assert decorated(5, 6) == 3
+    assert decorated(5, 6) == 3
+    assert decorated(1, 2) == 4
+    assert mocked_func.call_count == 4
